9919af643008c241e46df224e5453d3b23f32011
MayankDevil <mastermayank91@gmail.com>
2026-03-10 22:01:26 +0530

    add wekipedia clone

---
+++ web8/script.js
@@ -0,0 +1,14 @@
+/*
+-   Clone1 : "wikipedia" 
+-   Desgined | Developed by Mayank
+-   CopyRight by Wikipedia
+-   JavaScript : ./js/script.js
+*/
+try
+{
+    document.title = `MayankDevil`
+}
+catch(error)
+{
+    console.error(` something error in code ${error}`)
+}
